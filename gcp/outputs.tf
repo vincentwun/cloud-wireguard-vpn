92d2c87d5414eec284e59c0a1c4be16ebec91bde
vincentwun <144003077+vincentwun@users.noreply.github.com>
2025-10-03 15:50:53 +0800

    update version

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,18 +1,18 @@
 # Get ssh private key
 output "ssh_private_key" {
   description = "Private SSH key to access the WireGuard server."
-  value     = tls_private_key.ssh.private_key_openssh
-  sensitive = true
+  value       = tls_private_key.ssh.private_key_openssh
+  sensitive   = true
 }
 
 # Get server public IPv4 address
 output "server_public_ipv4" {
   description = "Public IPv4 address assigned to the WireGuard server."
-  value = local.ipv4_address
+  value       = local.ipv4_address
 }
 
 # Get server public IPv6 address
 output "server_public_ipv6" {
   description = "Public IPv6 address assigned to the WireGuard server."
-  value = local.ipv6_address
+  value       = local.ipv6_address
 }
